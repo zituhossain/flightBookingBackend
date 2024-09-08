ea4add942795f9583910883334c704e8fda14adf
zituhossain5 <zituhossain@gmail.com>
2024-09-08 18:50:16 +0600

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true,
   optionsSuccessStatus: 200,
 };
