36e5ca187e591ee77b514a9adc668fabd602fba3
zituhossain5 <zituhossain@gmail.com>
2024-09-08 21:47:06 +0600

    update index file

--- index.js
+++ index.js
@@ -22,6 +22,23 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://flight-bookingfrontend.vercel.app"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  next();
+});
+
 const PORT = process.env.PORT || 3000;
 
 // Routes
